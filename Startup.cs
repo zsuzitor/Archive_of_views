01816b99bd333f2b4465c05fdf0aca5f99e2de97
zsuzitor <avdosevstas@mail.ru>
2017-11-15 14:59:12 +0300

    start

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Archive_of_views.Startup))]
+namespace Archive_of_views
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
